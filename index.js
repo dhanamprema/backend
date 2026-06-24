bdb21cf8841a8925f501b93301929f739838ba35
dhanamprema <dhanamp766@gmail.com>
2026-06-24 14:15:51 +0530

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
